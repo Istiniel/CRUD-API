6e1e4cd3f25bf2bc8c42062b0a616bc357bbe0d6
Istiniel <eclipsio@mail.ru>
2023-07-02 12:54:11 +0300

    fix: fix webpack dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,10 @@
+const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.config.js')
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     compress: true,
     port: 9000,
